fb17f7182b6a854540734c066b2b9a9708d38c90
arjun-baidya <arjun.chandra.baidya@gmail.com>
2021-10-27 10:57:46 +0600

    done

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,10 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 import { useForm } from "react-hook-form";
 
 function AddService() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,reset } = useForm();
     const onSubmit = data => {
         console.log(data)
+        axios.post('http://localhost:5000/services',data)
+        .then(res=>{
+            if(res.data.insertedId){
+                alert('addedd successfully');
+                reset();
+            }
+        })
     };
 
     return (
@@ -14,6 +22,7 @@ function AddService() {
             <input {...register("Name", { required: true, maxLength: 20 })} placeholder="name" />
             <input {...register("price")} placeholder="price"/>
             <input {...register("description")} placeholder="description" />
+            <input {...register("image")} placeholder="image url" />
             <input type="submit" />
             </form>
         </div>
---
+++ src/Pages/Home/Services/ManageServices.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+function ManageServices() {
+    const [services, setServices] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/services')
+        .then(res=>res.json())
+        .then(data=> setServices(data))
+    },[])
+    const handleDelete = id =>{
+        const url = `http://localhost:5000/services/${id}`;
+        fetch(url,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            const remain = services.filter(service => service._id !==id)
+            setServices(remain)
+        })
+    }
+    return (
+        <div>
+            <h2>manage services</h2>
+            {
+                services.map(service=> <div key={service._id}>
+                    <h3>{service.name}</h3>
+                    <button onClick={()=>handleDelete(service._id)}>Delete</button>
+                </div>)
+            }
+        </div>
+    )
+}
+
+export default ManageServices
